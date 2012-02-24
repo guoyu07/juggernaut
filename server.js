f2aa290c0a21e86b40f764dffbdfcd9617b3a6a7
Binh Le <lebinh.it@gmail.com>
2012-02-24 10:54:30 +0700

    Refactored and added test script in index.html

--- server.js
+++ server.js
@@ -12,15 +12,15 @@ Juggernaut = require("./index");
 Juggernaut.listen(conf.port);
 
 // write pid file
-fs.writeFileSync(pidfile, process.pid);
+if (conf.daemonized)
+  fs.writeFileSync(pidfile, process.pid);
 
 // try to remove the pid file on exit
-var prepareExit = function(err) {
+var prepareExit = function() {
   if (path.existsSync(pidfile))
     fs.unlinkSync(pidfile);
-  process.exit(err? 1 : 0);
+  process.exit();
 };
 
 process.on('SIGINT', prepareExit);
 process.on('SIGTERM', prepareExit);
-process.on('uncaughtException', prepareExit);
